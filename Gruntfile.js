b727eeaffbc9db2c361bed266b11c24ce5446821
Jacek Wilczynski <jacekwilczynski5@gmail.com>
2017-11-21 23:23:43 +0100

    cosmetics

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,5 +27,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['watch']);
+	grunt.registerTask('default', ['sass', 'watch']);
 }
